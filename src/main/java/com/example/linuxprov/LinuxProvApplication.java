452f65f02aa51d18f40b7816fd8ef73e78159fd5
yun-jun-hyengh <77119415+yun-jun-hyengh@users.noreply.github.com>
2026-06-24 06:38:16 +0900

    Initial commit

---
+++ src/main/java/com/example/linuxprov/LinuxProvApplication.java
@@ -0,0 +1,13 @@
+package com.example.linuxprov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxProvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinuxProvApplication.class, args);
+	}
+
+}
